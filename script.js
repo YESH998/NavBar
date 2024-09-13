ed7075f288519d0316d0374e481172f2de9fdff5
yesh <yeshchoudhari68@gmail.com>
2024-09-13 11:18:05 +0530

    NavBar With Responsive

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSideBar(){
+    const sidebar =document.querySelector('.sideBar');
+    sidebar.style.display="flex";
+}
+function closeSideBar(){
+    const sideBar = document.querySelector('.sideBar');
+    sideBar.style.display="none";
+}
